03354e00795ce2b40f8b4adf3599ec5fc345ae55
KirtiJha <kirti.jha2025@gmail.com>
2025-04-03 22:45:36 +0530

    added contactHeader_section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import ContactHeader from './components/ContactHeader/ContactHeader';
 import Navber from './components/Navigation/Navber';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
      <Navber/>
+     <ContactHeader/>
     </>
   );
 }
---
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,12 @@
+import styles from "./ContactHeader.module.css";
+export default function ContactHeader() {
+  return (
+    <div className={`${styles.contact_section}`}>
+      <h1>CONTACT US</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nisi earum dicta maiores reiciendis corrupti quia nihil enim, cupiditate sed porro nesciunt eum voluptatum harum odit architecto sint quae beatae et fugiat assumenda suscipit sunt?.{" "}
+      </p>
+    </div>
+  );
+};
+
