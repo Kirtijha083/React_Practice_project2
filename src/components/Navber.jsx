55ae1caf9c4b950295b03e3ebb6054953797ee34
KirtiJha <kirti.jha2025@gmail.com>
2025-04-03 21:59:02 +0530

    added navbar

---
+++ src/components/Navber.jsx
@@ -0,0 +1,16 @@
+export default function Navber () {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="/images/logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+  //   <nav className={`${styles.navigation} container`}>
