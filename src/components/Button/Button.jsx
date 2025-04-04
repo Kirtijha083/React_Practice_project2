4a05035f17bb01fe176996d1cf6df451c6404d75
KirtiJha <kirti.jha2025@gmail.com>
2025-04-04 23:24:10 +0530

    added btn component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,22 @@
-import styles from "./Button.module.css";
-export default function Button ({ isOutline, icon, text, ...rest }){
+// import styles from "./Button.module.css";
+import { MdMessage } from "react-icons/md";
+// export default function Button ({ isOutline, icon, text, ...rest }){
+//   return (
+//     <button
+//       {...rest}
+//       className={isOutline ? styles.outline_btn : styles.primary_btn}
+//     >
+//       {icon}
+//       {text}
+//     </button>
+//   );
+// };
+
+export default function Button (){
   return (
-    <button
-      {...rest}
-      className={isOutline ? styles.outline_btn : styles.primary_btn}
-    >
-      {icon}
-      {text}
-    </button>
+    <div>
+      <MdMessage/>
+      Button
+    </div>
   );
 };
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,9 @@
+import Button from "../Button/Button";
 import styles from "./ContactForm.module.css";
 export default function ContactForm() {
   return (
     <section className={styles.container}>
-      <div className={styles.contact_form}> </div>
+      <div className={styles.contact_form}> <Button/>  </div>
         <div className={styles.top_btn}></div>
     </section>
 
