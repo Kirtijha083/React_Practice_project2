2f9a2e37f0378b0c312f93f5511bbea3ab8e42f3
KirtiJha <kirti.jha2025@gmail.com>
2025-04-05 15:08:13 +0530

    added 2nd btn

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,14 @@
 import styles from "./Button.module.css";
-import { MdMessage } from "react-icons/md";
+// import { MdMessage } from "react-icons/md";
 
-export default function Button (){
+export default function Button (prop){
   return (
     <button className={styles.primary_btn}>
-      <MdMessage fontSize="24px"/>
-      VIA SUPPORT CHAT
+       {prop.icon}
+    {prop.text}   {/*iss trah se jsx me js ka use krte h curly bracket ke help se */}
     </button>
   );
 };
+
+
+// here we use 2 time btn component...with using props taki repetation na ho
