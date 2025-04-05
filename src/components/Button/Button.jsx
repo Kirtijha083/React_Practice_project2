4e7dcc4eb9fbd68daf844572240379d399e05420
KirtiJha <kirti.jha2025@gmail.com>
2025-04-05 14:43:16 +0530

    added btn style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,22 +1,11 @@
-// import styles from "./Button.module.css";
+import styles from "./Button.module.css";
 import { MdMessage } from "react-icons/md";
-// export default function Button ({ isOutline, icon, text, ...rest }){
-//   return (
-//     <button
-//       {...rest}
-//       className={isOutline ? styles.outline_btn : styles.primary_btn}
-//     >
-//       {icon}
-//       {text}
-//     </button>
-//   );
-// };
 
 export default function Button (){
   return (
-    <div>
-      <MdMessage/>
-      Button
-    </div>
+    <button className={styles.primary_btn}>
+      <MdMessage fontSize="24px"/>
+      VIA SUPPORT CHAT
+    </button>
   );
 };
