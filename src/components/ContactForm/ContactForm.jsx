56e697766cbf19476a8f346db1fb6a225fbc3088
KirtiJha <kirti.jha2025@gmail.com>
2025-04-05 20:11:03 +0530

    added form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -12,6 +12,23 @@ export default function ContactForm() {
           <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
         </div>
         <Button isOutline={true} text="VIA EMAIL FORM" icon={<HiMail fontSize="24px" />} />
+
+
+        <form>
+          <div className={styles.form_control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="text">Text</label>
+            <textarea name="text" rows="8" />
+          </div>
+        </form>
+        
       </div>
       <div className={styles.contact_image}></div>
     </section>
